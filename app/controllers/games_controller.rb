97d022afe9735f9d753302f6cb2c22694d4c30ee
Nicole Ogawa <nogawa11@gmail.com>
2022-03-14 18:30:32 +0900

    fixed error, changed wording, and rerouted home page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,13 +17,13 @@ class GamesController < ApplicationController
     @grid = params['letters'].downcase.chars
     if word?(@answer) && checker(@contain, @grid)
       @score = @results['length']
-      @message = "Congratulations! <b>#{@answer.upcase}</b> is a valid English world! Your score is #{@score}.".html_safe
+      @message = "Congratulations! <b>#{@answer.upcase}</b> is a valid English world! Your score is #{@score} pts. for this round.".html_safe
+      session[:score] += @score
     elsif word?(@answer) && !checker(@contain, @grid)
       @message = "Sorry but <b>#{@answer.upcase}</b> can't be built out of #{params['letters'].upcase.gsub(' ', ', ')}".html_safe
     else
       @message = "Sorry but <b>#{@answer.upcase}</b> does not seem to be a valid English word...".html_safe
     end
-    session[:score] += @score
   end
 
   def checker(answer_array, grid_array)
